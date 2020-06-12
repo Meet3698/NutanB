21b512d4221aa3fd063ee2a07e0d3837d48ffc12
jack5972 <meetp6041@gmail.com>
2020-06-12 15:14:17 +0530

    'commit'

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const router = express.Router()
 const User = mongoose.model('UserSchema')
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
 
 router.post('/register', (req, res) => {
 
@@ -30,17 +31,42 @@ router.post('/register', (req, res) => {
     })
 })
 
-router.post('/login',async(req,res)=>{
-    const user = await User.collection.find({email:req.body.email}).toArray()
+router.post('/login', async (req, res) => {
+    const user = await User.collection.find({ email: req.body.email }).toArray()
 
-    bcrypt.compare(req.body.password, user[0].password, (err, result) =>{
-        if(result==true){
-            res.send("success")
+    bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+        if (result == true) {
+            jwt.sign({ email: req.body.email }, 'wolfpack.com', (err, token) => {
+                res.json({
+                    token: token,
+                    message : 'success'
+                })
+            })
         }
-        else{
+        else {
             res.send("fail")
         }
     });
 })
+
+router.post('/verify',(req,res)=>{
+    jwt.verify(req.body.token, 'wolfpack.com',(err,authData)=>{
+        if(err){
+            res.json({message : false})
+        }else{
+            res.send({message : true})
+        }
+    })
+})
+
+// function verifyToken(req,res,next){
+//     const bearerToken = req.body.token
+//     if(typeof bearerToken !== 'undefined'){
+//         req.token = bearerToken
+//         next()
+//     }else{
+//         res.sendS
+//     }
+// }
 
 module.exports = router
